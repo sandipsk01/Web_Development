1f4d6fa09df29949e4017287342dd7f2a8c19fda
“Sandip <sandipkalekar808@gmail.com>
2022-07-21 01:51:20 +0530

    Added input statement, Output statements, learnt about premetive and non premetive datatypes

--- JAVASCRIPT/Chapter1/scriptFile.js
+++ JAVASCRIPT/Chapter1/scriptFile.js
@@ -1,4 +1,26 @@
-function giveAnswer(){
+function giveAnswer(name,answer){
+    //outputStatements
     // alert("Yes")
-    document.write("dffddfdjgdjgdfgfdfgfdgdgdgdg")
+    // document.write("dffddfdjgdjgdfgfdfgfdgdgdgdg")
+    // var myAnwer=name +"says"+answer
+    // alert(myAnwer) //window.alert
+    //console.log
+    var x=window.prompt();
+
+
+    //sandip=1
+    //others=2
+
+    if((name=='Sandip'&& x==1)
+    ||(name=='Other'&& x==2)){
+        var x=document.getElementById('feedback');
+        x.innerHTML=name+" says that "+answer;
+    }
+    else{
+        var x=document.getElementById('errorStatement');
+        x.innerHTML="Invalid Credentials";
+    }
+    // input statements
+    // window.prompt()
+    // window.confirm()
 }
